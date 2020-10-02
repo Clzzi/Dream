cf00c74ac270dc1fc8dada58574d580b0f289b6a
Clzzi <sonmin1407@dgsw.hs.kr>
2020-10-03 01:23:05 +0900

    Dream - make same symbol

--- Note.js
+++ Note.js
@@ -90,6 +90,14 @@ const symbol1 = Symbol('id');
 const symbol2 = Symbol('id');
 console.log(symbol1 === symbol2); //false
 
+//동일한 심볼을 만들고 싶을때 
+const gSymbol1 = Symbol.for('id');
+const gSymbol2 = Symbol.for('id');
+console.log(gSymbol1 === gSymbol2); //true
+console.log(`value: ${symbol1.description}, type: ${typeof symbol1}`)
+// 심볼을 출력할때는 .description으로 문자열로 출력해야함
+
+
 
 
 
