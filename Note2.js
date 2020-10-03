ac17b60447f79b89a5151b469dfd8ebada4daece
Clzzi <sonmin1407@dgsw.hs.kr>
2020-10-03 16:46:42 +0900

    Dream - Number

--- Note2.js
+++ Note2.js
@@ -4,3 +4,11 @@ console.log('1' + 2);
 console.log(`string literals: 1 + 2 = ${1 + 2}`);
 console.log('Minjae\'s \nbook ');
 
+// 2. Numeric operators -- 숫자연산
+console.log(1 + 1); //더하기 (add)
+console.log(1 - 1); //빼기 (substract)
+console.log(1 / 1); // 나누기 (devide)
+console.log(1 * 1); //곱하기 (multiply)
+console.log(5 % 2); //나머지 (remainder)
+console.log(2 ** 3); //제곱 (exponentiation)
+
