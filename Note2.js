98c7a98787a8a7a9e1c835f615c7b30f7a928cb0
Clzzi <sonmin1407@dgsw.hs.kr>
2020-10-03 16:47:12 +0900

    Dream - Increment & decrement

--- Note2.js
+++ Note2.js
@@ -12,3 +12,11 @@ console.log(1 * 1); //곱하기 (multiply)
 console.log(5 % 2); //나머지 (remainder)
 console.log(2 ** 3); //제곱 (exponentiation)
 
+// 3. Increment and decrement operators -- 전.후위 연산자
+let counter = 2;
+const preIncrement = ++counter; 
+//전위 연산자 (=> counter = counter + 1; preIncrement = counter )
+const postIncrement = counter++;
+//후위 연산자 (=> postIncrement = counter; counter = counter + 1 )
+// + , - 둘다 같음 
+
