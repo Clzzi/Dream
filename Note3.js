b98e147487f0aa19d2450cf14ae5ff8b43e3d0de
Clzzi <sonmin1407@dgsw.hs.kr>
2020-10-12 20:24:44 +0900

    Dream - Function

---
+++ Note3.js
@@ -0,0 +1,29 @@
+/* Function 
+- fundamental building block in the program
+- subprogram can be used multiple time
+- performs a task or calculates a value
+
+1. Function declaration
+function name(param1, param2) { body... return; }
+one function === one thing 
+naming: doSomethnig, command, verb
+e.g. createCardAndPoint -> createCard, createPoint
+function is object in JS
+*/
+function printHello() {
+    console.log('Hello');
+};
+printHello();
+
+function log(message) {
+    console.log(message);
+};
+log('hi'); 
+
+
+
+
+
+
+
+
