bb64752e8930e0ee6188b83994bcdfb1f4b12d99
Clzzi <sonmin1407@dgsw.hs.kr>
2020-10-12 20:35:07 +0900

    Dream - Getter and Setter

--- Note4.js
+++ Note4.js
@@ -6,7 +6,7 @@ Js calsses
 - introduced in ES6
 - syntactical sugar over prototype-based inheritance
 
-1. Class declartions
+1. Class declarations
 */
 class Person {
     //constructor
@@ -26,3 +26,22 @@ const minjae = new Person('minjae', '21');
 console.log(minjae.age);
 console.log(minjae.name);
 minjae.speak();
+
+//2. Getter and setters
+class User {
+    constructor(firstName, lastName, age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    get age() {
+        return this._age;
+    }
+
+    set age(value) {
+        this._age = value < 0 ? 0 : value;
+    }
+}
+const user1 = new User('Steve','Job',-1);
+console.log(user1.age);
