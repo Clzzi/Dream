3ec38fcff91d086cce9e05d40221246148c64356
Clzzi <sonmin1407@dgsw.hs.kr>
2020-10-14 20:54:04 +0900

    Dream - pop / push / shift / unshift

--- Note6.js
+++ Note6.js
@@ -22,4 +22,27 @@ for(let key of fruits)
     console.log(key);
 
 //c. forEach
-fruits.forEach((fruit) => console.log(fruit))
+fruits.forEach((fruit) => console.log(fruit));
+
+// 4. Addition, deletion, copy
+// push : add an item to the end
+fruits.push('grape');
+
+// pop : remove an item from the end
+fruits.pop();
+
+//unshift :  add an item to the benigging
+fruits.unshift('apple');
+
+//shift : remove an item from the benigging
+fruits.shift();
+
+// shift, unshift are slower than pop, push
+// spilce : remove an item by index position
+fruits.push('orange');
+fruits.splice(1,1); // 1번위치에서 부터 1개 삭제
+fruits.splice(1,1,'a','b'); // 1번위치에서 1개 삭제한후에 그위치에 a와b를 넣어줌
+
+// combine two arrays
+const fruits2 = ['man','woman'];
+const newFruits = fruits.concat(fruits2); // 둘이 합쳐짐
