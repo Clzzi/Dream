1497bb5b415565a6b64018750d01a26a1a2da983
Clzzi <sonmin1407@dgsw.hs.kr>
2020-10-14 20:52:36 +0900

    Dream - Index position & declartion

---
+++ Note6.js
@@ -0,0 +1,12 @@
+//Array
+// 1. Declartion
+const arr1 = new Array();
+const arr2 = [1, 2];
+
+//2. Index postion
+const fruits = ['apple','banana'];
+console.log(fruits);
+console.log(fruits.length);
+console.log(fruits[0]); //apple
+console.log(fruits[33]); //undefined
+console.log(fruits[fruits.length-1]);
