3ad81df1341944a5df30cc2f94c207c91c128600
Clzzi <sonmin1407@dgsw.hs.kr>
2020-10-15 20:16:14 +0900

    Dream - Join();

---
+++ Note7.js
@@ -0,0 +1,5 @@
+//Q1. Make a string out of an array
+// 배열을 문자열로 바꾸어 주는 함수 -> join();
+const fruits = ['apple', 'banana','orange'];
+const result = fruits.join();
+console.log(result); //apple,banana,orang
