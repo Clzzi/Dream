dff73a3a0a7abde0b3a882d6c2929aa4888eb33e
Clzzi <sonmin1407@dgsw.hs.kr>
2020-10-15 20:17:28 +0900

    Dream - slice();

--- Note7.js
+++ Note7.js
@@ -15,4 +15,10 @@ console.log(result2);
 // 배열안에 들어있는 아이템의 순서를 거꾸로 바꾸어 주는 함수 -> reverse();
 const array = [1,2,3,4,5];
 array.reverse();
-console.log(array)
+console.log(array);
+
+//Q4. Make new array without the first two elements
+// 배열의 특정한 부분을 리턴해주는 함수 -> slice(0, 3); 0부터 2까지임
+const array2 = [1,2,3,4,5];
+const result3 = array2.slice(2, 5); // 배열의2번위치부터 4번위치까지를 리턴해줌
+console.log(result3); // [3, 4, 5
