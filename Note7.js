60835019f2fcf07cfa9a7ea17c8cd118f938c4fb
Clzzi <sonmin1407@dgsw.hs.kr>
2020-10-15 20:16:32 +0900

    Dream - split();

--- Note7.js
+++ Note7.js
@@ -2,4 +2,11 @@
 // 배열을 문자열로 바꾸어 주는 함수 -> join();
 const fruits = ['apple', 'banana','orange'];
 const result = fruits.join();
-console.log(result); //apple,banana,orang
+console.log(result); //apple,banana,orange
+
+//Q2. Make an array out of a string
+// 문자열을 배열로 바꾸어 주는 함수 -> split();
+const fruits2 = 'apple,grape,coco';
+const result2 = fruits2.split(',', 2); // ['apple','grape'];
+ // split(' ',limit); 이 있음 limit은 해도되고 안해도 됨
+console.log(result2)
