c697fb58bce267b1b89445c0236369f57ae1ffe5
Clzzi <sonmin1407@dgsw.hs.kr>
2020-10-19 22:08:45 +0900

    Dream - async & sync

--- Note9.js
+++ Note9.js
@@ -8,6 +8,25 @@ setTimeout(function() {
 console.log('3');
 // 1 3 2  
 /* arrow function.ver
---> setTimeout(() => console.log('2'), 1000); */ 
+--> setTimeout(() => console.log('2'), 1000); */  
+
+// Synchronous callback(즉각적으로 수행함)
+function printImmediately(print) {
+    print();
+}
+printImmediately(() => console.log('hello')); 
+
+// Asynchronous callback(언제 실행될지 모름)
+function printWithDelay(print, timeout) {
+    setTimeout(print, timeout); 
+}
+printImmediately(() => console.log('async callback'), 3000); 
+// arrow function == print / 3000 == timeout 이되어 setTimeout이 완성됨
+
+
+
+
+
+
 
 
