2c4b103360439f7dc4ee103801d1cf31f78a6afd
Clzzi <sonmin1407@dgsw.hs.kr>
2020-10-19 22:09:07 +0900

    Dream - callback Hell

--- Note9.js
+++ Note9.js
@@ -21,7 +21,62 @@ function printWithDelay(print, timeout) {
     setTimeout(print, timeout); 
 }
 printImmediately(() => console.log('async callback'), 3000); 
-// arrow function == print / 3000 == timeout 이되어 setTimeout이 완성됨
+// arrow function == print / 3000 == timeout 이되어 setTimeout이 완성됨.
+
+// Callback Hell Example
+class UserStorage {
+    loginUser(id, password, onSucess, onError) {
+        setTimeout( () => {
+            if((id === 'minjae' && password === 'alswo') || (id === 'sonminjae' && password === 'thsalswo')) {
+                onSucess(id);
+            } else {
+                onError(new Error('not found'));
+            }
+        },2000);
+    }
+
+    getRoles(user, onSucess, onError) {
+        setTimeout( () => {
+            if( user === 'minjae') {
+                onSucess({ name: 'minjae', role: 'admin' });
+            } else {
+                onError(new Error('no access'));
+            }
+        }, 1000);
+    }
+}
+
+const userStorage = new UserStorage();
+const id = prompt('enter your id');
+const password = prompt('enter your password');
+userStorage.loginUser(
+    id,
+    password,
+    user => { //loginUser의 onSucess
+        userStorage.getRoles(
+            user,
+            userWithRole => { //getRoles의 onSucess 
+                alert(`Hello ${userWithRole.name}, you have a ${userWithRole.role} role`);
+            },
+            error => { //getRoles의 onError
+                console.log(error);
+            }
+        );
+    },
+    error => { //loginUser의 onError
+        console.log(error);
+    }
+);
+
+
+
+
+
+
+
+
+
+
 
 
 
