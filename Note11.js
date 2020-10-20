57dd6a4575cac2a1d1e61e08cd3c68cb9bbd72a0
Clzzi <sonmin1407@dgsw.hs.kr>
2020-10-20 21:54:55 +0900

    Dream - await

--- Note11.js
+++ Note11.js
@@ -25,3 +25,58 @@ function fetchUser() {
         resolve('minjae'); --> resolve로 해주면 promise가 fulfiled 상태가됨
     });
 } */
+
+// 2. await
+function delay(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+/* function getBanana() {
+    return delay(3000)
+    .then(() => 'Banana')
+} promise를 리턴하는 기본형*/ 
+
+async function getApple() {
+    await delay(1000);
+    throw 'error';
+    return 'Apple';
+}
+
+async function getBanana() {
+    await delay(1000);
+    return 'Banana';
+}
+ 
+async function pickFruits() {
+    const applePromise = getApple(); // Promise를 만들게되면 바로 실행되므로 1초딜레이가 없어짐
+    const bananaPromise = getBanana();
+    const apple = await applePromise;
+    const banana = await bananaPromise;
+    return `${apple} + ${banana}`; // --> 1초후에 로그나타남 (병렬) 
+}
+/*
+[1] function pickFruits() {
+    return getApple().then(apple => {
+        return getBanana().then(banana => `${apple} + ${banana}`);
+    });
+} 
+pickFruits().then(console.log); 
+--> 2초후에 콘솔나타남
+
+[2] async function pickFruits() {
+    const apple = await getApple();
+    const banana = await getBanana();
+    return `${apple} + ${banana}`; // --> 2초후에 로그나타남 (직렬)
+} */
+
+
+
+
+
+
+
+
+
+
+
+
